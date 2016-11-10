8de2f9884381cd14a19c96d7680c3d0b2f04063b
tompetrus <tom.petrus.be@gmail.com>
2016-11-10 08:56:03 +0100

    Fixes in javascript and added ir_to_xml header back in
    IR2MSEXCEL.js :
    - namespaced the functions so they won't live on window anymore. They're
    under the "excel_gpv" namespace now.
    - Ensures that "$" is "apex.jQuery", avoiding problems with multiple
    jQuery libraries.
    - colWidths: checks if static id is passed in, throws an error if not
    - download icon: didn't account for image directory as "/i/" was
    hardcoded, now uses the correct variable.
    - Uses apex.util.htmlBuilder to create the html to be appended
    IR_TO_MSEXCEL:
    - changed the javascript calls to call the functions in the "excel_gpv"
    namespace
    IR_TO_XML:
    - The previous commit had the package header removed from the code,
    which caused the installation of packages to fail as a whole
    install_all_packages:
    - recreated this file since IR_TO_XML and IR_TO_MSEXCEL were changed
    README:
    - changed a few typos
    Plugin sql:
    - export of the plugin to account for changes in javascript file

--- IR2MSEXCEL.js
+++ IR2MSEXCEL.js
@@ -1,40 +1,80 @@
-function getColWidthsDelimeteredString(p_region_static_id) {  
-    var colWidthsDelimeteredString = "";    
-    var region_selector;
-    if(p_region_static_id)
-        {
-            region_selector = '#' + p_region_static_id + '_data_panel';
-        }
-    $('div' + region_selector + ' table.a-IRR-table th:not(".a-IRR-header--group")').parent().first().find('th').each(function( index,elmt ){    
-      colWidthsDelimeteredString = colWidthsDelimeteredString + $(elmt).width() + "\,";  
-    });
-    return colWidthsDelimeteredString;
-}
+(function(parent, $, undefined){
+  
+	if (parent.excel_gpv === undefined) {
 
-function addDownloadXLSXIcon(plugin_id_in,p_region_static_id) {     
-  $('body').on( "dialogopen", function( event, ui ) {
-     if($('span.ui-dialog-title').text() === apex.lang.getMessage( "APEXIR_DOWNLOAD")) {
-        var $dialog_window = $(event.target);
-        var current_region_id = $dialog_window.attr('id').match(/(.+)_dialog_js/)[1]; 
-        var re = new RegExp(p_region_static_id, 'g'); 
-         
-        if(!p_region_static_id || current_region_id.match(re) != null){        
-          $dialog_window.find('table.a-IRR-dialogTable tbody td a').closest('tr').append('<tr><td nowrap="nowrap"><a href="javascript:get_excel_gpv(' + "'" + current_region_id + "','" + plugin_id_in +  "'" +')"><img src="/i/ws/download_xls_64x64.gif" alt="XLSX" title="XLSX"></a></td></tr>');
-          $dialog_window.find('table.a-IRR-dialogTable tbody td span').closest('tr').append( '<tr><td align="center" nowrap="nowrap"><span>XLSX</span></td></tr>');
-        }    
-     } 
-  }); 
-}
+		parent.excel_gpv = function () {
+      //private
+      function getColWidthsDelimeteredString ( p_region_static_id ) {
+        if ( $.type(p_region_static_id) !== "string"){
+          throw("Expecting a string as static ID (arugment: p_region_static_id)");
+        };
 
-function get_excel_gpv(p_region_static_id,plugin_id_in) {
-  apex.navigation.redirect(
-        "wwv_flow.show?p_flow_id=" + $('#pFlowId').val() 
-                   + "&p_flow_step_id=" + $('#pFlowStepId').val() 
-                   + "&p_instance="+ $v('pInstance')  
-                   + "&p_request=" + "PLUGIN=" + plugin_id_in
-                   + "&x01=" + $('#pFlowId').val()
-                   + "&x02=" + $('#pFlowStepId').val()
-                   + "&x03=" + p_region_static_id
-                   + "&x04=" + getColWidthsDelimeteredString(p_region_static_id)
-   );     
-}
+        var colWidthsDelimeteredString = "";
+        var region_selector = '#' + p_region_static_id + '_data_panel';
+
+        $('div' + region_selector + ' table.a-IRR-table th:not(".a-IRR-header--group")').parent().first().find('th').each(function( index,elmt ){
+          colWidthsDelimeteredString = colWidthsDelimeteredString + $(elmt).width() + "\,";
+        });
+        return colWidthsDelimeteredString;
+      }
+
+      //exposed
+      function getExcel ( p_region_static_id, plugin_id_in ) {
+        apex.navigation.redirect(
+              "wwv_flow.show?p_flow_id=" + $v('pFlowId')
+                         + "&p_flow_step_id=" + $v('pFlowStepId')
+                         + "&p_instance="+ $v('pInstance')
+                         + "&p_request=" + "PLUGIN=" + plugin_id_in
+                         + "&p_debug=" + $v("pdebug")
+                         + "&x01=" + $v('pFlowId')
+                         + "&x02=" + $v('pFlowStepId')
+                         + "&x03=" + p_region_static_id
+                         + "&x04=" + getColWidthsDelimeteredString(p_region_static_id)
+         );
+      }
+      
+      function addDownloadXLSXIcon ( plugin_id_in, p_region_static_id ) {
+        $('body').on( "dialogopen", function( event, ui ) {
+          if($('span.ui-dialog-title').text() === apex.lang.getMessage( "APEXIR_DOWNLOAD")) {
+            var $dialog_window = $(event.target);
+            var current_region_id = $dialog_window.attr('id').match(/(.+)_dialog_js/)[1];
+            var re = new RegExp(p_region_static_id, 'g');
+            
+            var html = apex.util.htmlBuilder();
+            html.markup('<tr>')
+                .markup('<td')
+                  .attr('nowrap', 'nowrap')
+                .markup('>')
+                .markup('<a')
+                  .attr('href','javascript:excel_gpv.getExcel(' + "'" + current_region_id + "','" + plugin_id_in +  "'" +');')
+                .markup('>')
+                .markup('<img')
+                  .attr('src', apex_img_dir+'ws/download_xls_64x64.gif')
+                  .attr('alt', 'XLSX')
+                  .attr('title', 'XLSX')
+                .markup(' />')
+                .markup('</a>')
+                .markup('</td>')
+                .markup('</tr>');
+            
+            if(!p_region_static_id || current_region_id.match(re) != null){            
+              $dialog_window.find('table.a-IRR-dialogTable tbody td a')
+                .closest('tr')
+                .append( html.toString() );
+                
+              $dialog_window.find('table.a-IRR-dialogTable tbody td span')
+                .closest('tr')
+                .append( '<tr><td align="center" nowrap="nowrap"><span>XLSX</span></td></tr>');
+            };
+          }
+        });
+      }
+      
+      return {
+        getExcel:getExcel
+      , addDownloadXLSXIcon: addDownloadXLSXIcon
+      };
+  
+		}();
+	}
+})(window, apex.jQuery)
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 GPV Interactive Report to MSExcel
 =================================
 
-Getting result of your Interactive Report in Microsoft Excel. 
+Getting results of your Interactive Report in Microsoft Excel. 
 
 ##About
 
-The "GPV Interactive Report to MSExcel v2" APEX plugin lets you getting results of your Interactive Report in XSML-format, ready to be used in Microsoft Excel.
+The "GPV Interactive Report to MSExcel v2" APEX plugin lets you get results of your Interactive Report in XSML-format, ready to be used in Microsoft Excel.
 
 Many features of Interactive Reports are supported:
  $  (new in v2.0) APEX 5 support
@@ -15,7 +15,7 @@ Many features of Interactive Reports are supported:
  $  Control Break
  $  Computations
  $  Aggregations
- $  No BI Publishe Required
+ $  No BI Publisher Required
 
 The minimum required APEX-version is 5.0.
 Documentation, downloads, and other useful information can be found [here](http://glebovpavel.github.io/Description_IR_TO_XSLX/).
